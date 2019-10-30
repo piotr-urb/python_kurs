f7522fef1ecb30be41bd8bde536bd2c7a5c3b9a7
piotr-urb <piotr_urbanczyk@wp.pl>
2019-10-30 23:57:46 +0100

    Dodano zadania

---
+++ Lesson_3_if_elif_else_for_while/Homeworks/01_Case_nr_4.py
@@ -0,0 +1,12 @@
+#(part: if, elif, Else)
+#Zadanie nr 4:
+#Utwórz zmienną przechowującą dowolny ciąg znaków.
+#Sprawdź czy utworzony string jest dłuższy niż 5 znaków oraz czy zawiera literę a.
+#Jeśli zawiera, wszystkie litery a podmień na z i wyświetl powstały napis.
+
+print("Podaj dowolny string:")
+string = str(input())
+if len(string) > 5:
+    print("String is longest then 5 letters")
+else:
+    print("String is shorter then 5 letters")

4dd530ceff27290488515bb109c1661e81be0172
piotr-urb <piotr_urbanczyk@wp.pl>
2019-12-03 23:28:13 +0100

    aktualizacja +homework

--- Lesson_16_Homeworks/Lesson_4_Kolekcje/03_Case_nr_6.py
+++ Lesson_16_Homeworks/Lesson_4_Kolekcje/03_Case_nr_6.py
@@ -5,10 +5,3 @@ days = {'Jan': 31, 'Feb': 28, 'Mar': 31, 'Apr': 30, 'May': 31, 'Jun': 30, 'Jul':
 
 days = {'Jan': 31, 'Feb': 28, 'Mar': 31, 'Apr': 30, 'May': 31, 'Jun': 30, 'Jul': 31, 'Aug': 31, 'Sept': 30}
 
-days_list = list(days.values())
-
-without_duplicates = set(days_list)
-
-list_without_duplicates = list(without_duplicates)
-
-print("\n", list_without_duplicates)
